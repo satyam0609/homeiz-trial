de85a9385e699ea44d1e50bb9c54f14681d01bd1
satyam0609 <satyam.kumar.e008@gmail.com>
2026-03-23 22:37:05 +0530

    added focused border to search box

--- src/components/searchbox.tsx
+++ src/components/searchbox.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 interface Props {
   placeholder?: string;
@@ -5,17 +6,21 @@ interface Props {
 }
 
 const SearchBox = ({ placeholder, onChange }: Props) => {
+  const [active, setActive] = React.useState(false);
   return (
     <input
+      onFocus={() => setActive(true)}
+      onBlur={() => setActive(false)}
       onChange={(e) => onChange && onChange(e.target.value)}
       type="text"
       placeholder={placeholder}
-      className="w-full px-5 py-3 rounded-full border outline-none
+      className={`w-full px-5 py-3 rounded-full  outline-none
              text-sm font-bold text-text-primary
-             placeholder:text-gray-600"
+             placeholder:text-gray-600 border ${
+               active ? "border-blue-500" : "border-[rgb(230,228,224)]"
+             }`}
       style={{
         backgroundColor: "rgb(241, 240, 237)",
-        borderColor: "rgb(230, 228, 224)",
       }}
     />
   );
